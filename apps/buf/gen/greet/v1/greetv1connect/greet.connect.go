1b7839726e5b5c6b0b83844b8faeb4309aa973ff
navigaid <navigaid@gmail.com>
2024-01-16 10:28:02 +0000

    echo: cache put/post body for /*

--- apps/buf/gen/greet/v1/greetv1connect/greet.connect.go
+++ apps/buf/gen/greet/v1/greetv1connect/greet.connect.go
@@ -5,12 +5,13 @@
 package greetv1connect
 
 import (
-	connect "connectrpc.com/connect"
 	context "context"
 	errors "errors"
-	v1 "github.com/webteleport/ufo/apps/buf/gen/greet/v1"
 	http "net/http"
 	strings "strings"
+
+	connect "connectrpc.com/connect"
+	v1 "github.com/webteleport/ufo/apps/buf/gen/greet/v1"
 )
 
 // This is a compile-time assertion to ensure that this generated file and the connect package are
