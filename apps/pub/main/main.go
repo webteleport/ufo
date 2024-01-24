3aa7b065ab9b0e9e0e1b02e5772b58d9c1d78fbc
navigaid <navigaid@gmail.com>
2024-01-24 12:34:41 +0000

    pub: fix spin

--- apps/pub/main/main.go
+++ apps/pub/main/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"github.com/fermyon/spin/sdk/go/v2/http"
-	"github.com/webteleport/ufo/apps/ser/handler"
+	"github.com/webteleport/ufo/apps/pub/handler"
 )
 
 func init() {
-	http.Handle(handler.Handler().ServeHTTP)
+	http.Handle(handler.Handler(".").ServeHTTP)
 }
 
 func main() {}
