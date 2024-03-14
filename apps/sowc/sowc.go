bacd9bad5639920f225b6c5075d17462146089d9
navigaid <navigaid@gmail.com>
2024-03-14 15:02:29 +0000

    sowc: update hints for http_proxy

--- apps/sowc/sowc.go
+++ apps/sowc/sowc.go
@@ -24,23 +24,33 @@ func Arg1(args []string, fallback string) string {
 	return fallback
 }
 
-func Run(args []string) error {
-	addr := Arg0(args, ":8123")
-	remote := Arg1(args, "wss://sows.ufo.k0s.io")
-	log.Println("socks5 listening on", addr)
-	log.Println("remote socks5+wss", remote)
-
+// CAVEAT: curl recognizes only lowercase http_proxy, not HTTP_PROXY
+func proxyHints(addr string) {
 	log.Println("# bash")
 	println(fmt.Sprintf("export HTTP_PROXY=http://127.0.0.1%s HTTPS_PROXY=http://127.0.0.1%s", addr, addr))
+	println(fmt.Sprintf("export http_proxy=http://127.0.0.1%s https_proxy=http://127.0.0.1%s", addr, addr))
 	println(fmt.Sprintf("export HTTP_PROXY=socks5h://127.0.0.1%s HTTPS_PROXY=socks5h://127.0.0.1%s", addr, addr))
+	println(fmt.Sprintf("export http_proxy=socks5h://127.0.0.1%s https_proxy=socks5h://127.0.0.1%s", addr, addr))
 
 	log.Println("# cmd")
 	println(fmt.Sprintf("set HTTP_PROXY=http://127.0.0.1%s HTTPS_PROXY=http://127.0.0.1%s", addr, addr))
+	println(fmt.Sprintf("set http_proxy=http://127.0.0.1%s https_proxy=http://127.0.0.1%s", addr, addr))
 	println(fmt.Sprintf("set HTTP_PROXY=socks5h://127.0.0.1%s HTTPS_PROXY=socks5h://127.0.0.1%s", addr, addr))
+	println(fmt.Sprintf("set http_proxy=socks5h://127.0.0.1%s https_proxy=socks5h://127.0.0.1%s", addr, addr))
 
 	log.Println("# powershell")
 	println(fmt.Sprintf("$env:HTTP_PROXY='http://127.0.0.1%s'; $env:HTTPS_PROXY='http://127.0.0.1%s'", addr, addr))
+	println(fmt.Sprintf("$env:http_proxy='http://127.0.0.1%s'; $env:https_proxy='http://127.0.0.1%s'", addr, addr))
 	println(fmt.Sprintf("$env:HTTP_PROXY='socks5h://127.0.0.1%s'; $env:HTTPS_PROXY='socks5h://127.0.0.1%s'", addr, addr))
+	println(fmt.Sprintf("$env:http_proxy='socks5h://127.0.0.1%s'; $env:https_proxy='socks5h://127.0.0.1%s'", addr, addr))
+}
+
+func Run(args []string) error {
+	addr := Arg0(args, ":8123")
+	remote := Arg1(args, "wss://sows.ufo.k0s.io")
+	log.Println("[arg0] socks5 listening on", addr)
+	log.Println("[arg1] remote socks5+wss", remote)
+	proxyHints(addr)
 
 	ln, err := net.Listen("tcp", addr)
 	if err != nil {
