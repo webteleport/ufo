8c4bc70c3498f76a14dddd2fcac33e48c239cfee
navigaid <navigaid@gmail.com>
2024-03-17 05:35:03 +0000

    gotip: override version via GOTIP=go1.19.13
    $ GOTIP=go1.19.13 ./bin/ufo gotip version
    go version go1.19.13 linux/arm64

--- apps/gotip/gotip.go
+++ apps/gotip/gotip.go
@@ -2,12 +2,28 @@ package gotip
 
 import (
 	"os"
+	"strings"
 
 	"github.com/btwiuse/dl/version"
 )
 
 func Run(args []string) error {
-	os.Args = append([]string{"gotip"}, args...)
-	version.RunTip()
+	if gotip := os.Getenv("GOTIP"); gotip == "" {
+		os.Args = append([]string{"gotip"}, args...)
+		version.RunTip()
+	} else {
+		if !strings.HasPrefix(gotip, "go") {
+			gotip = "go" + gotip
+		}
+		return RunVersion(gotip)(args)
+	}
 	return nil
+}
+
+func RunVersion(v string) func([]string) error {
+	return func(args []string) error {
+		os.Args = append([]string{v}, args...)
+		version.Run(v)
+		return nil
+	}
 }
