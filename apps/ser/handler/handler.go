773be9e45de6f427e37d1dc6d3560ba20d973710
navigaid <navigaid@gmail.com>
2024-03-21 07:54:14 +0000

    ser: use better.FileServer

--- apps/ser/handler/handler.go
+++ apps/ser/handler/handler.go
@@ -4,15 +4,23 @@ import (
 	"expvar"
 	"net/http"
 
+	"github.com/btwiuse/better"
 	"github.com/webteleport/utils"
 )
 
 func Handler(s string) http.Handler {
-	handler := http.FileServer(http.Dir(s))
+	handler := better.FileServer(http.Dir(s))
 	handler = utils.GzipMiddleware(handler)
 	handler = utils.GinLoggerMiddleware(handler)
 	mux := http.NewServeMux()
 	mux.Handle("/", handler)
 	mux.HandleFunc("/debug/vars", expvar.Handler().ServeHTTP)
 	return mux
+}
+
+func RootHandler() http.Handler {
+	handler := Handler("/")
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		handler.ServeHTTP(w, r)
+	})
 }
