d2ffbcde33851dcc110fb7295c2b12efb771ca89
navigaid <navigaid@gmail.com>
2024-12-09 14:39:48 +0000

    v0.6.19: pocket: make relay optional based on the presence of HOST

--- apps/pocket/pocket.go
+++ apps/pocket/pocket.go
@@ -2,20 +2,10 @@
 package pocket
 
 import (
-	"log"
 	"os"
-	"strings"
 
 	"github.com/pocketbase/pocketbase"
-	"github.com/pocketbase/pocketbase/core"
 	"github.com/pocketbase/pocketbase/plugins/jsvm"
-	"github.com/pocketbase/pocketbase/tools/hook"
-	"github.com/webteleport/relay"
-	"github.com/webteleport/utils"
-)
-
-var (
-	HOST = utils.EnvHost("^localhost$")
 )
 
 func Run(args []string) error {
@@ -39,37 +29,7 @@ func Run(args []string) error {
 	})
 
 	// registers the relay middleware
-	app.OnServe().Bind(&hook.Handler[*core.ServeEvent]{
-		Func: func(se *core.ServeEvent) error {
-			log.Println("starting the relay server", "HOST", HOST)
-
-			store := relay.NewSessionStore()
-
-			if os.Getenv("LOGGIN") != "" {
-				store.Use(utils.GinLoggerMiddleware)
-			}
-
-			mini := relay.NewWSServer(HOST, store)
-
-			se.Router.BindFunc(func(re *core.RequestEvent) error {
-				isPocketbaseHost := mini.IsRootExternal(re.Event.Request)
-				isPocketbaseAPI := strings.HasPrefix(re.Event.Request.URL.Path, "/api/")
-				isPocketbaseUI := strings.HasPrefix(re.Event.Request.URL.Path, "/_/")
-				isPocketbase := isPocketbaseHost && (isPocketbaseAPI || isPocketbaseUI)
-
-				// route non pocketbase requests to relay
-				if !isPocketbase {
-					mini.ServeHTTP(re.Event.Response, re.Event.Request)
-					return nil
-				}
-
-				return re.Next()
-			})
-
-			return se.Next()
-		},
-		Priority: -99999, // execute as early as possible
-	})
+	app.OnServe().Bind(RelayHook)
 
 	return app.Start()
 }
---
+++ apps/pocket/relay.go
@@ -0,0 +1,56 @@
+package pocket
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/tools/hook"
+	"github.com/webteleport/relay"
+	"github.com/webteleport/utils"
+)
+
+var HOST = utils.EnvHost("")
+
+var RelayHook = &hook.Handler[*core.ServeEvent]{
+	Func: func(se *core.ServeEvent) error {
+		if HOST == "" {
+			return se.Next()
+		}
+
+		log.Println("starting the relay server", "HOST", HOST)
+
+		store := relay.NewSessionStore()
+
+		if os.Getenv("LOGGIN") != "" {
+			store.Use(utils.GinLoggerMiddleware)
+		}
+
+		mini := relay.NewWSServer(HOST, store)
+
+		se.Router.BindFunc(func(re *core.RequestEvent) error {
+			r := re.Event.Request
+			isPocketbaseHost := mini.IsRootExternal(r)
+			isAPI := strings.HasPrefix(r.URL.Path, "/api/")
+			isUI := strings.HasPrefix(r.URL.Path, "/_/")
+			isPocketbase := isPocketbaseHost && (isAPI || isUI)
+
+			if os.Getenv("DEBUG") != "" {
+				log.Println(fmt.Sprintf("isPocketbase (%v) := isPocketbaseHost (%v) && (isAPI (%v) || isUI (%v))\n", isPocketbase, isPocketbaseHost, isAPI, isUI))
+			}
+
+			// route non pocketbase requests to relay
+			if !isPocketbase {
+				mini.ServeHTTP(re.Event.Response, re.Event.Request)
+				return nil
+			}
+
+			return re.Next()
+		})
+
+		return se.Next()
+	},
+	Priority: -99999, // execute as early as possible
+}
