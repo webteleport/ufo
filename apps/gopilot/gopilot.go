d9dfa2d7418379c01dcaceca65cc1eaceb3804fe
navigaid <navigaid@gmail.com>
2025-01-02 13:37:11 +0000

    gopilot: add gin logger middleware

--- apps/gopilot/gopilot.go
+++ apps/gopilot/gopilot.go
@@ -10,6 +10,7 @@ import (
 func Run(args []string) error {
 	handler := gopilot.Handler()
 	handler = utils.GzipMiddleware(handler)
+	handler = utils.GinLoggerMiddleware(handler)
 	handler = utils.AllowAllCorsMiddleware(handler)
 	arg0 := apps.Arg0(args, apps.RELAY)
 	return wtf.Serve(arg0, handler)
